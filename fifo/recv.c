b49b7935583de6d8937208497889eeffb2747fb0
shadab khan <shadab0138@gmail.com>
2020-11-06 16:01:34 +0530

    inter process communication FIFO

---
+++ fifo/recv.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+int main(int argc, char const *argv[])
+{
+    int fd;
+    unsigned char rbuff[64];
+    fd=open("desd",O_RDONLY);
+    read(fd,rbuff,64);
+    printf("String is:%s\n",rbuff);
+    close(fd);
+    return 0;
+}
---
+++ fifo/sender.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+int main(int argc, char const *argv[])
+{
+    int fd;
+    fd=open("desd",O_WRONLY);
+    write(fd,"cdac\n",5);
+    close(fd);
+    return 0;
+}
